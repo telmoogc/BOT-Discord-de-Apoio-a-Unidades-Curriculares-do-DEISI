1c7b402308238a135522ad034fb3a64269105bc5
telmoogc <telmo.98@hotmail.com>
2022-01-05 21:16:25 +0000

    Teste2: base dados

--- database/_insert.js
+++ database/_insert.js
@@ -4,41 +4,41 @@ const { Client } = require("discord.js");
 const client = new Client();
 
 async function insertData(){
-    await db.connect();
+    db.connect();
 
 
     // Criar aluno
     const queryStudent = "INSERT INTO student (stu_name) VALUES ($1)";
 
     client.on('message', (message) => {
-        const newStudent = await db.query(queryStudent, [message.author.username]);
+        const newStudent = db.query(queryStudent, [message.author.username]);
     });
 
-    await db.query(queryStudent, [string]);
+    db.query(queryStudent, [string]);
 
 
     // Criar resposta do aluno
     const queryAnswer = "INSERT INTO answer (stu_answer) VALUES ($1)";
 
     client.on('message', (message) => {
-        const newAnswer = await db.query(queryAnswer, [message.content]);
+        const newAnswer = db.query(queryAnswer, [message.content]);
     });
 
-    await db.query(queryAnswer, [string]);
+    db.query(queryAnswer, [string]);
 
 
     //Adicionar respostas aos alunos
     const queryStudentAnswer = "INSERT INTO student_answer (student_id, answer_id) VALUES ($1, $2)";
 
     client.on('message', (message) => {
-        const newStudent = await db.query(queryStudentAnswer, [message.author.id, message.id]);
+        const newStudent = db.query(queryStudentAnswer, [message.author.id, message.id]);
     });
 
-    await db.query(queryAnswer, [string, string]);
+    db.query(queryAnswer, [string, string]);
 
 
-    
-    await db.end();
+
+    db.end();
     console.log("data inserted successfully!")
 }
 
