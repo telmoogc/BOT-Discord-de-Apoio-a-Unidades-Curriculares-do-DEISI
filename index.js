6b0d5ce97de1b9f150950fc62c7a79bde43f5483
telmoogc <telmo.98@hotmail.com>
2022-01-17 23:29:31 +0000

    bd connect 3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const Eris = require("eris");
 const insertData = require('./database/_insert');
+const db = require('./_database.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 let bot = new Eris(process.env.BOT_TOKEN);
@@ -11,6 +12,8 @@ let prefix = "!";
 
 client.once('ready', () => {
     console.log('Estou online');
+    db.connect();
+    console.log("DB connectada");
 });
 
 
@@ -29,7 +32,23 @@ bot.on("messageCreate", async message => {
 
 bot.on("messageCreate", async message => {
     console.log("ESTOU AQUI!!!!!");
-    insertData;
+
+
+    const queryStudent = "INSERT INTO student (stu_name) VALUES ($1)";
+
+    console.log("queryStudent debug 1");
+
+    client.on('message', (message) => {
+        const newStudent = db.query(queryStudent, [message.author.username]);
+    });
+
+    console.log("queryStudent debug 2");
+
+    db.query(queryStudent, [string]);
+
+    console.log("queryStudent debug 3");
+
+
     console.log("SUPOSTAMENTE INSERI OS DADOS");
 });
 
