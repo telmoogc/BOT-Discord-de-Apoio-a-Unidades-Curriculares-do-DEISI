b3096a4c9c2590c13ebaa6c2ad5213d81453cda4
telmoogc <telmo.98@hotmail.com>
2022-03-07 21:11:29 +0000

    env vars, webApp

---
+++ webApp/webApp.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/main-menu", function(req, res){
+    res.sendFile(__dirname + "/main-menu.html");
+});
+
+app.get("/uc-details", function(req, res){
+    res.sendFile(__dirname + "/uc-details.html");
+});
+
+app.listen(process.env.PORT || 80, function(){
+    console.log("Server started on port 3000");
+});
