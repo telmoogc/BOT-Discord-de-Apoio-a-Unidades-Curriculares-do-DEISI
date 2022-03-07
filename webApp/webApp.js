50e3a0388fa33c56eb6e152254f89b641d211f78
telmoogc <telmo.98@hotmail.com>
2022-03-07 23:27:45 +0000

    style e imagens na webApp

--- webApp/webApp.js
+++ webApp/webApp.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res){
