3bd25df5a615649359038e71b0c5587797c65195
telmoogc <telmo.98@hotmail.com>
2022-03-08 00:08:20 +0000

    Erro imagens/icons

--- webApp/webApp.js
+++ webApp/webApp.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
 
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}), express.static(__dirname));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
